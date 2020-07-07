b1dec0c2ba7ca21e8ace82ed21fa5ba878d65d10
Luis Felipe <vilella.luisfelipe@gmail.com>
2020-07-07 19:31:42 -0300

    adding fields pydantic types

--- pydantic2graphene/fields.py
+++ pydantic2graphene/fields.py
@@ -51,7 +51,7 @@ def _get_type_mapping():
         pydantic.EmailStr: graphene.String,
         pydantic.NameEmail: graphene.String,
         pydantic.color.Color: graphene.String,
-        pydantic.Json: graphene.types.json.JSONString,
+        # pydantic.Json: graphene.types.json.JSONString,
         pydantic.HttpUrl: graphene.String,
         pydantic.UUID1: graphene.String,
         pydantic.UUID3: graphene.String,
