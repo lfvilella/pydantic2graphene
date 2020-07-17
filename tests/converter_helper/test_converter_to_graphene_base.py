2793ada41b622c76212dadb43f550b2f979bf3e0
Luis Felipe <vilella.luisfelipe@gmail.com>
2020-07-17 19:50:21 -0300

    test extra fields

--- tests/converter_helper/test_converter_to_graphene_base.py
+++ tests/converter_helper/test_converter_to_graphene_base.py
@@ -32,3 +32,22 @@ class TestConverterToGrapheneBase:
             }
         """
         assert normalize_sdl(value) == normalize_sdl(expected_value)
+
+    def test_include_extra_fields_to_the_schema(
+        self, normalize_sdl,
+    ):
+        class HumanConverterExtra(pydantic2graphene.ConverterToGrapheneBase):
+            class Config:
+                model = Human
+                use_cache = False
+
+            some_extra_field = graphene.Int(required=False)
+
+        value = HumanConverterExtra.as_class()
+        expected_value = """
+            type HumanGql {
+                someExtraField: Int
+                name: String!
+            }
+        """
+        assert normalize_sdl(value) == normalize_sdl(expected_value)
