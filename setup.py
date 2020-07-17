b8848c2da8358f0b34e6b442fd1fc1e1c52daa0d
Luis Felipe <vilella.luisfelipe@gmail.com>
2020-07-17 19:32:24 -0300

    'pydantic>=1.0,<=1.6.1'

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ version = re.search(r"v\d+(\.\d+)+", version_txt).group()
 
 requirements = [
     'graphene>=1.0,<=2.1.8',
-    'pydantic>=1.5,<=1.6.1',
+    'pydantic>=1.0,<=1.6.1',
 ]
 
 github_url = "https://github.com/lfvilella/pydantic2graphene"
