5c1fc662d0f5d71f55c596712143eed06b780c79
Luis Felipe <vilella.luisfelipe@gmail.com>
2020-07-18 00:39:38 -0300

    Test string field

--- tests/fields/test_fields.py
+++ tests/fields/test_fields.py
@@ -20,14 +20,6 @@ def to_pydantic_class(field_type):
 
 
 class TestTypeMappingPydantic2Graphene:
-    def test_string_field(self, normalize_sdl):
-        value = pydantic2graphene.to_graphene(to_pydantic_class(str))
-        expected_value = """
-            type FakeGql {
-                field: String!
-            }
-        """
-        assert normalize_sdl(value) == normalize_sdl(expected_value)
 
     def test_int_field(self, normalize_sdl):
         value = pydantic2graphene.to_graphene(to_pydantic_class(int))
---
+++ tests/fields/test_string.py
@@ -0,0 +1,84 @@
+import graphene
+import pydantic
+import pydantic2graphene
+
+
+class MyModel(pydantic.BaseModel):
+    field1: str
+    field2: str = "abc"
+
+
+def test_schema_declaration(normalize_sdl):
+    value = pydantic2graphene.to_graphene(MyModel)
+    expected_value = """
+        type MyModelGql {
+            field1: String!
+            field2: String
+        }
+    """
+    assert normalize_sdl(value) == normalize_sdl(expected_value)
+
+
+def test_query_usage():
+    MyModelGql = pydantic2graphene.to_graphene(MyModel)
+
+    class Query(graphene.ObjectType):
+        hello = graphene.Field(MyModelGql)
+
+        @staticmethod
+        def resolve_hello(parent, info):
+            return MyModel(field1="lets", field2="rock")
+
+    schema = graphene.Schema(query=Query)
+
+    query = """
+    query {
+        hello {
+            f1: field1
+            field2
+        }
+    }
+    """
+    value = schema.execute(query).to_dict()
+    expected_value = {"data": {"hello": {"f1": "lets", "field2": "rock"}}}
+
+    assert value == expected_value
+
+
+def test_input_usage():
+    MyModelGql = pydantic2graphene.to_graphene(MyModel)
+    MyModelInputGql = pydantic2graphene.to_graphene(
+        MyModel, graphene.InputObjectType
+    )
+
+    class Query(graphene.ObjectType):
+        hello = graphene.Field(MyModelGql, imp=MyModelInputGql())
+
+        @staticmethod
+        def resolve_hello(parent, info, imp):
+            return imp
+
+    schema = graphene.Schema(query=Query)
+
+    query = """
+    query {
+        h1: hello (imp: {field1: "123"}) {
+            f1: field1
+            f2: field2
+        }
+
+        h2: hello (imp: {field1: "go", field2: "python"}) {
+            field1
+            field2
+        }
+    }
+    """
+    value = schema.execute(query).to_dict()
+    expected_value = {
+        "data": {
+            "h1": {"f1": "123", "f2": "abc"},
+            "h2": {"field1": "go", "field2": "python"},
+        }
+    }
+
+    assert value == expected_value
